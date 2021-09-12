e044d01b71cdd1ab10442c00015336e981d004db
Artemiy Matchin <art16ws@gmail.com>
2021-09-12 21:56:03 +0300

    Added retrofit api to AppModule.kt

--- app/src/main/java/com/artemiymatchin/testrentateamapp/di/AppModule.kt
+++ app/src/main/java/com/artemiymatchin/testrentateamapp/di/AppModule.kt
@@ -2,11 +2,14 @@ package com.artemiymatchin.testrentateamapp.di
 
 import android.app.Application
 import androidx.room.Room
+import com.artemiymatchin.testrentateamapp.api.RetrofitApi
 import com.artemiymatchin.testrentateamapp.data.UsersDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -24,5 +27,18 @@ object AppModule {
 
     @Provides
     fun provideTaskDAO(db: UsersDatabase) = db.userDao()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(RetrofitApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofitApi(retrofit: Retrofit): RetrofitApi =
+        retrofit.create(RetrofitApi::class.java)
 
 }
