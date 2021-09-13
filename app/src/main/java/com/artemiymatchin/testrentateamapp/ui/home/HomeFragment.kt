13a1d2b6a428df6d96827be27739a952bfd94d07
Artemiy Matchin <art16ws@gmail.com>
2021-09-13 18:40:18 +0300

    Adapter for users recycler view

--- app/src/main/java/com/artemiymatchin/testrentateamapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/artemiymatchin/testrentateamapp/ui/home/HomeFragment.kt
@@ -16,5 +16,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentHomeBinding.bind(view)
+
+
     }
 }
---
+++ app/src/main/java/com/artemiymatchin/testrentateamapp/ui/home/UsersAdapter.kt
@@ -0,0 +1,41 @@
+package com.artemiymatchin.testrentateamapp.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.artemiymatchin.testrentateamapp.data.User
+import com.artemiymatchin.testrentateamapp.databinding.UserShortCardBinding
+
+class UsersAdapter : ListAdapter<User, UsersAdapter.UsersViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
+        val binding = UserShortCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UsersViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class UsersViewHolder(private val binding: UserShortCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.apply {
+                firstNameField.text = user.first_name
+                lastNameField.text = user.last_name
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<User>() {
+
+        override fun areItemsTheSame(oldItem: User, newItem: User) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: User, newItem: User) =
+            oldItem == newItem
+    }
+}
